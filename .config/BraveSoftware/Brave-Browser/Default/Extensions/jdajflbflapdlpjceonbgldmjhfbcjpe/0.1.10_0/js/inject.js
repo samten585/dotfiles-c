35ba3be4258aa411c809fccfe1e8781c83f2e820
samten585 <samten585@github.com>
2026-03-06 13:06:27 +0200

    improving and deleting unnecessary files

--- .config/BraveSoftware/Brave-Browser/Default/Extensions/jdajflbflapdlpjceonbgldmjhfbcjpe/0.1.10_0/js/inject.js
+++
@@ -1,1 +0,0 @@
-document.body.addEventListener("click",e=>{window.postMessage({target:"turbo-type",action:"body-click"},"*")});
--- .config/Code - OSS/User/History/18f2b8ad/MeVc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-current=$(hyprctl getoption general:layout -j | jq -r ".str")
-
-if [ "$current" = "dwindle" ]; then
-    hyprctl keyword general:layout master
-else
-    hyprctl keyword general:layout dwindle
-fi
